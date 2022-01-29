55ddd88366bb2ba61f351b433a00750e7c9a4855
Freymanis <nfreymanis@gmail.com>
2022-01-29 16:58:21 +0300

    Initial Commit

---
+++ GitApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitApp
+//
+//  Created by Nikita Freymanis on 29.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
