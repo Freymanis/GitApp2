9fa7077aec8fa6badc369b5e6560c27a764e1183
Freymanis <nfreymanis@gmail.com>
2022-01-29 17:00:30 +0300

    code 1

--- GitApp/AppDelegate.swift
+++ GitApp/AppDelegate.swift
@@ -13,7 +13,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+// comit 1
+        
         return true
     }
 
